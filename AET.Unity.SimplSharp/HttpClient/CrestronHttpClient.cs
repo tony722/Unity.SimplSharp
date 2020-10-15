45f04fafd1e586747934ef97be27a033891049be
Tony Evert <tony722@gmail.com>
2020-10-15 16:18:16 -0700

    Improve error handling in HttpClient

--- AET.Unity.SimplSharp/HttpClient/CrestronHttpClient.cs
+++ AET.Unity.SimplSharp/HttpClient/CrestronHttpClient.cs
@@ -21,9 +21,14 @@ namespace AET.Unity.SimplSharp.HttpClient {
 
     public string Get(string url, IEnumerable<KeyValuePair<string, string>> additionalHeaders) {
       if (Debug == 1) CrestronConsole.PrintLine("Unity.CrestronPooledHttpClient.PostAsync({0})", url);
-      var results = pool.Get(url);
-      if (Debug == 1) CrestronConsole.PrintLine("Results status({0}): {1}", results.Status, results.Content);
-      return results.Content;
+      try {
+        var results = pool.Get(url);
+        if (Debug == 1) CrestronConsole.PrintLine("Results status({0}): {1}", results.Status, results.Content);
+        return results.Content;
+      } catch (Exception ex) {
+        ErrorMessage.Error("Unity.CrestronHttpClient.Get({0}) Error: {1}", url, ex.Message);
+      }
+      return string.Empty;
     }
 
     public string Post(string url, string contents) {
@@ -32,9 +37,14 @@ namespace AET.Unity.SimplSharp.HttpClient {
 
     public string Post(string url, string contents, IEnumerable<KeyValuePair<string, string>> additionalHeaders) {
       if (Debug == 1) CrestronConsole.PrintLine("Unity.CrestronPooledHttpClient.Post({0})\r\nContents: {1}", url, contents);
-      var results = pool.Post(url, contents, additionalHeaders);
-      if (Debug == 1) CrestronConsole.PrintLine("Results status({0}): {1}", results.Status, results.Content);
-      return results.Content;
+      try {
+        var results = pool.Post(url, contents, additionalHeaders);
+        if (Debug == 1) CrestronConsole.PrintLine("Results status({0}): {1}", results.Status, results.Content);
+        return results.Content;
+      } catch (Exception ex) {
+        ErrorMessage.Error("Unity.CrestronHttpClient.Post({0}) Error: {1}\r\nContents: {2}", url, ex.Message, contents);
+      }
+      return string.Empty;
     }
   }
 }
