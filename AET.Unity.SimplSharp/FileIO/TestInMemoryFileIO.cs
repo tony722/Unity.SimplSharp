308909ab2e463a22a273f1eb4976636b3027158a
Tony Evert <tony722@gmail.com>
2024-06-04 11:01:55 -0700

    TestInMemoryFileIO: Fix Exists check to work correctly. Add exception to ReadAllText when file does not exist

--- AET.Unity.SimplSharp/FileIO/TestInMemoryFileIO.cs
+++ AET.Unity.SimplSharp/FileIO/TestInMemoryFileIO.cs
@@ -3,16 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Crestron.SimplSharp.CrestronIO;
 
 namespace AET.Unity.SimplSharp.FileIO {
   public class TestInMemoryFileIO : IFileIO {
     public TestInMemoryFileIO() {
       Files = new Dictionary<string, string>();
     }
-    public Dictionary<string, string> Files { get; private set;} 
-    public string ReadAllText(string fileName) { return Files[fileName];  }
+    public Dictionary<string, string> Files { get; private set;}
+
+    public string ReadAllText(string fileName) {
+      try {return Files[fileName];}
+      catch { throw new FileNotFoundException("Could not open file '{0}'."); }
+    }
     public void WriteText(string fileName, string data) { Files[fileName] = data; }
     public void WriteText(string fileName, string data, bool useVersioning) { Files[fileName] = data; }
-    public bool Exists(string fileName) { return fileName != null || Files.Keys.Contains(fileName); }
+    public bool Exists(string fileName) { return fileName != null && Files.Keys.Contains(fileName); }
   }
 }
